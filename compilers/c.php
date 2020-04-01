73df32b94f02355153eb83306e8ea3ea75609418
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-04-01 13:27:20 +0530

    Update c.php

--- compilers/c.php
+++ compilers/c.php
@@ -1,8 +1,18 @@
 <?php
-	$CC="gcc";
+header('Access-Control-Allow-Origin: *');  
+        
+    
+        $postdata = file_get_contents("php://input");
+        $request = json_decode($postdata);
+        @$email = $request->email;
+		// echo $email;
+
+		$decode =urldecode($email);
+		
+		$CC="gcc";
 	$out="timeout 5s ./a.out";
-	@$code=$_GET["code"];
-	@$input=$_GET["input"];
+	$code=$decode;
+	$input="";
 	$filename_code="main.c";
 	$filename_in="input.txt";
 	$filename_error="error.txt";
@@ -39,11 +49,12 @@
 			$output=shell_exec($out);
 		}
 		//echo "<pre>$output</pre>";
-        echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
-	}
+		@$myObj->name = $output;
+		$myJSON = json_encode($myObj);
+		echo $myJSON;	}
 	else if(!strpos($error,"error"))
 	{
-		echo "<pre>$error</pre>";
+		// echo "<pre>$error</pre>";
 		if(trim($input)=="")
 		{
 			$output=shell_exec($out);
@@ -54,35 +65,40 @@
 			$output=shell_exec($out);
 		}
 		//echo "<pre>$output</pre>";
-                echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
-	}
+		@$myObj->name = $output;
+		$myJSON = json_encode($myObj);
+		echo $myJSON;	}
 	else
 	{
-		echo "<pre>$error</pre>";
+		@$myObj->name = $error;
+		$myJSON = json_encode($myObj);
+		echo $myJSON;		
+		
 		$check=1;
 	}
 	$executionEndTime = microtime(true);
 	$seconds = $executionEndTime - $executionStartTime;
-	$seconds = sprintf('%0.2f', $seconds);
-	echo "<pre>Compiled And Executed In: $seconds s</pre>";
+	// $seconds = sprintf('%0.2f', $seconds);
+	// echo "<pre>Compiled And Executed In: $seconds s</pre>";
 	if($check==1)
 	{
-		echo "<pre>Verdict : CE</pre>";
+		// echo "<pre>Verdict : CE</pre>";
 	}
 	else if($check==0 && $seconds>3)
 	{
-		echo "<pre>Verdict : TLE</pre>";
+		// echo "<pre>Verdict : TLE</pre>";
 	}
 	else if(trim($output)=="")
 	{
-		echo "<pre>Verdict : WA</pre>";
+		// echo "<pre>Verdict : WA</pre>";
 	}
 	else if($check==0)
 	{
-		echo "<pre>Verdict : AC</pre>";
+		// echo "<pre>Verdict : AC</pre>";
 	}
 	exec("rm $filename_code");
 	exec("rm *.o");
 	exec("rm *.txt");
 	exec("rm $executable");
+    
 ?>
