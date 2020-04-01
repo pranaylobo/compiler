1a4eaeab01671d46724359cec6d8cdcf752a226a
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-04-01 22:10:19 +0530

    Update c.php

--- compilers/c.php
+++ compilers/c.php
@@ -49,10 +49,10 @@ header('Access-Control-Allow-Origin: *');
 			$out=$out." < ".$filename_in;
 			$output=shell_exec($out);
 		}
-		//echo "<pre>$output</pre>";
-		@$myObj->name = $output;
-		$myJSON = json_encode($myObj);
-		echo $myJSON;	}
+		echo "<pre>$output</pre>";
+// 		@$myObj->name = $output;
+// 		$myJSON = json_encode($myObj);
+// 		echo $myJSON;	}
 	else if(!strpos($error,"error"))
 	{
 		// echo "<pre>$error</pre>";
@@ -65,10 +65,11 @@ header('Access-Control-Allow-Origin: *');
 			$out=$out." < ".$filename_in;
 			$output=shell_exec($out);
 		}
-		//echo "<pre>$output</pre>";
-		@$myObj->name = $output;
-		$myJSON = json_encode($myObj);
-		echo $myJSON;	}
+		echo "<pre>$output</pre>";
+// 		@$myObj->name = $output;
+// 		$myJSON = json_encode($myObj);
+// 		echo $myJSON;	
+	}
 	else
 	{
 		@$myObj->name = $error;
