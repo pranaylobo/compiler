8cb80b1c88fbe7debb8b861c1d44591ad29ad1f9
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2020-05-26 21:48:48 +0530

    Update c.php

--- compilers/c.php
+++ compilers/c.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
+
 header('Access-Control-Allow-Origin: *');  
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
