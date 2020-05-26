4011b4325bdc7942a4750a2b9e0fce79c5a4d372
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2020-05-27 00:13:40 +0530

    Update python27.php

--- compilers/python27.php
+++ compilers/python27.php
@@ -24,26 +24,78 @@
 	exec("chmod -R 777 $filename_code");  
 	exec("chmod 777 $filename_error");	
 	shell_exec($command_error);
-	
-	$error=file_get_contents($filename_error);
+	exec("chmod -R 777 $executable");
 
+	$error=file_get_contents($filename_error);
 	if(trim($error)=="")
 	{
 		if(trim($input)=="")
 		{
-			$output=shell_exec($command);
+			$output=shell_exec($out);
 		}
 		else
 		{
-			$command=$command." < ".$filename_in;
-			$output=shell_exec($command);
+			$out=$out." < ".$filename_in;
+			$output=shell_exec($out);
+			
+			
 		}
-		echo "<pre>$output</pre>";
+		//echo "<pre>$output</pre>";
+			//   echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
+			@$myObj->name = $output;
+$myJSON = json_encode($myObj);
+echo $myJSON;	
+	}
+	else if(!strpos($error,"error"))
+	{
+		echo "<pre>$error</pre>";
+		if(trim($input)=="")
+		{
+			$output=shell_exec($out);
+		}
+		else
+		{
+			$out=$out." < ".$filename_in;
+			$output=shell_exec($out);
+		}
+						// echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
+						@$myObj->name = $output;
+$myJSON = json_encode($myObj);
+echo $myJSON;	
 	}
 	else
 	{
-		echo "<pre>$error</pre>";
+		// echo "<pre>$error</pre>";
+		@$myObj->name = $error; 
+$myJSON = json_encode($myObj);
+echo $myJSON;		
+		$check=1;
 	}
-	exec("rm $filename_code");
+	$executionEndTime = microtime(true);
+	$seconds = $executionEndTime - $executionStartTime;
+	$seconds = sprintf('%0.2f', $seconds);
+	// echo "<pre>Compiled And Executed In: $seconds s</pre>";
+
+
+	if($check==1)
+	{
+		// echo "<pre>Verdict : CE</pre>";
+	}
+	else if($check==0 && $seconds>3)
+	{
+		// echo "<pre>Verdict : TLE</pre>";
+	}
+	else if(trim($output)=="")
+	{
+		// echo "<pre>Verdict : WA</pre>";
+	}
+	else if($check==0)
+	{
+		// echo "<pre>Verdict : AC</pre>";
+	}
+
+    exec("rm $filename_code");
+	exec("rm *.o");
 	exec("rm *.txt");
+	exec("rm $executable");
 ?>
