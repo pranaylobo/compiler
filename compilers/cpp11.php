30fb4689437f9dd48412073ecfdbf8149d3b0e8d
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2020-08-25 03:19:05 +0530

    Update cpp11.php

--- compilers/cpp11.php
+++ compilers/cpp11.php
@@ -1,6 +1,8 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');  
+header('Content-Type: text/html; charset=UTF-8');
+
+header('Access-Control-Allow-Origin: *'); 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 @$email = $request->email; 
